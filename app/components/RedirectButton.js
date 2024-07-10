9ea465482fecdee47c065adb3275895df68ea105
joncoronel <59675970+joncoronel@users.noreply.github.com>
2024-07-10 16:43:51 -0700

    refactor: Update RedirectButton component to use server action and client router
    The RedirectButton component was modified to use a server action for redirecting to the private page, and the client router for the same purpose. This change provides flexibility in handling the redirect functionality.

--- app/components/RedirectButton.js
+++ app/components/RedirectButton.js
@@ -1,17 +1,26 @@
 "use client";
 
-// import { redirectPrivate } from "../actions";
+import { redirectPrivate } from "../actions";
 import { useRouter } from "next/navigation";
 
 export default function RedirectButton() {
   const router = useRouter();
   return (
-    <button
-      onClick={() => {
-        router.push("/private");
-      }}
-    >
-      Redirect to private
-    </button>
+    <>
+      <button
+        onClick={() => {
+          redirectPrivate();
+        }}
+      >
+        Redirect to private through server action
+      </button>
+      <button
+        onClick={() => {
+          router.push("/private");
+        }}
+      >
+        Redirect to private through client router
+      </button>
+    </>
   );
 }
