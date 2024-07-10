591bd301d8d73761416ac11481e8ed87aeb75812
joncoronel <59675970+joncoronel@users.noreply.github.com>
2024-07-10 16:13:14 -0700

    refactor: Update middleware to use NextResponse.next

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,7 @@
 import { NextResponse, type NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  return NextResponse.next({
-    request,
-  });
+  return NextResponse.next();
 }
 
 export const config = {
